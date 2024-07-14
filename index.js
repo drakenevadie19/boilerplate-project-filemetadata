bec35c143d46a469bc38f486b80deb552adbace1
drakenevadie19 <drakennievadje19@gmail.com>
2024-07-13 23:35:47 -0500

    First Change : adding endpoint in index.js and install multer package

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ require('dotenv').config()
 
 var app = express();
 
+// Middleware to parse JSON bodies
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +16,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", (req, res) => {
 
+})
 
 
 const port = process.env.PORT || 3000;
